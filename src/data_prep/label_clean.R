b2a3998e647749479a1ac8573e5845c37952408a
juliehabets <j.n.g.habets@tilburguniversity.edu>
2022-11-08 16:42:00 +0100

    updtae

---
+++ src/data_prep/label_clean.R
@@ -0,0 +1,5 @@
+library(data.table)
+library(dplyr)
+
+# load dataset
+total_label <- fread("../../gen/temp/total_label.csv")
--- src/data_prep/label_to_data.R
+++ src/data_prep/label_to_data.R
@@ -16,12 +16,43 @@ users_1month <- users_1month[, -c(1, 3:8, 10)]
 users_1month$users_1month <- users_1month[!duplicated(users_1month), ]
 
 # joining tracks
-users_1month_tracks <- full_join(users_1month, match_tracks, by = "track_name")
-users_1month_tracks <- users_1month_tracks[!duplicated(users_1month_tracks), ]
+users_tracks <- full_join(users_1month, match_tracks, by = "track_name")
 
 # clean
-users_1month_tracks <- users_1month_tracks[, -5]
-names(users_1month_tracks)[2] <- c("artist")
+users_tracks <- users_tracks[, -c(1,4,5)]
+names(users_tracks)[1] <- c("artist")
+users_tracks <- users_tracks[!duplicated(users_tracks), ]
 
 # joining artists
-users_1month_tracks_artists <- full_join(users_1month_tracks, match_artists, by = "artist")
+users_artists <- full_join(users_1month, match_artists, by = "artist")
+
+# clean
+users_artists <- users_artists[, -c(1,4)]
+users_artists <- users_artists[!duplicated(users_artists), ]
+
+# check NAs
+na <- users_tracks %>% filter(is.na(label))
+na <- na %>% filter(!(artist %in% users_tracks$artist))
+
+rm(na)
+# NAs in labels from users_tracks are all from artists that are represented through other songs
+
+# Add datasets together
+total_label <- users_tracks
+total_label$label[is.na(total_label$label)] <- users_artists$label[match(total_label$artist,users_artists$artist)][which(is.na(total_label$label))]
+
+# clean
+total_label <- total_label[!duplicated(total_label), ]
+
+# checking NAs 
+na <- total_label %>% filter(is.na(label))
+na <- na %>% filter(!(artist %in% users_artists$artist))
+
+rm(na)
+# NAs in labels are all from artists that are represented through other songs
+
+# remove NA values 
+total_label <- total_label %>% filter(!is.na(label))
+
+#write to csv
+write.csv(total_label, "../../gen/temp/total_label.csv")
